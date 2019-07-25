f2592863dd51d637512ef2223aed3c920b95222a
임종호 <ejsvkvhdlsxm@naver.com>
2019-07-25 15:17:27 +0900

    로그인 기능 작업

--- src/main/java/kr/or/yi/food_mgm_program/ui/LoginFrame.java
+++ src/main/java/kr/or/yi/food_mgm_program/ui/LoginFrame.java
@@ -5,23 +5,30 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import kr.or.yi.food_mgm_program.dao.ManagerDao;
+import kr.or.yi.food_mgm_program.daoImpl.ManagerDaoImpl;
+import kr.or.yi.food_mgm_program.dto.Manager;
+
 import java.awt.event.FocusListener;
 import java.awt.event.FocusEvent;
 
 public class LoginFrame extends JFrame implements ActionListener, FocusListener {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-
+	private JTextField tfId;
+	private JPasswordField pfPass;
+	private JButton btnLogin;
+	private JButton btnMLogin;
+	private ManagerDao dao;
 
 	public LoginFrame() {
+		dao = new ManagerDaoImpl();
 		initComponents();
 	}
 	private void initComponents() {
@@ -34,50 +41,70 @@ public class LoginFrame extends JFrame implements ActionListener, FocusListener 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.addFocusListener(this);
-		textField.setText("아이디를 입력하세요");
-		textField.setBounds(42, 149, 250, 44);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		tfId = new JTextField();
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(42, 195, 250, 44);
-		contentPane.add(passwordField);
+		tfId.addFocusListener(this);
 		
-		btnNewButton = new JButton("로그인");
-		btnNewButton.addActionListener(this);
-		btnNewButton.setBounds(42, 278, 117, 44);
-		contentPane.add(btnNewButton);
+		tfId.setBounds(42, 149, 250, 44);
+		contentPane.add(tfId);
+		tfId.setColumns(10);
 		
-		btnNewButton_1 = new JButton("관리자 로그인");
-		btnNewButton_1.addActionListener(this);
-		btnNewButton_1.setBounds(169, 278, 123, 44);
-		contentPane.add(btnNewButton_1);
+		pfPass = new JPasswordField();
+		pfPass.setBounds(42, 195, 250, 44);
+		contentPane.add(pfPass);
+		
+		btnLogin = new JButton("로그인");
+		btnLogin.addActionListener(this);
+		btnLogin.setBounds(42, 278, 117, 44);
+		contentPane.add(btnLogin);
+		
+		btnMLogin = new JButton("관리자 로그인");
+		btnMLogin.addActionListener(this);
+		btnMLogin.setBounds(169, 278, 123, 44);
+		contentPane.add(btnMLogin);
 	}
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnNewButton_1) {
+		if (e.getSource() == btnMLogin) {
 			actionPerformedBtnNewButton_1(e);
 		}
-		if (e.getSource() == btnNewButton) {
+		if (e.getSource() == btnLogin) {
 			actionPerformedBtnNewButton(e);
 		}
 	}
 	protected void actionPerformedBtnNewButton(ActionEvent e) {
 	}
 	public void focusGained(FocusEvent e) {
-		if (e.getSource() == textField) {
+		if (e.getSource() == tfId) {
 			focusGainedTextField(e);
 		}
 	}
 	public void focusLost(FocusEvent e) {
-		textField.setText("아이디를 입력하세요");
+		
 	}
 	protected void focusGainedTextField(FocusEvent e) {
-		textField.setText("");
+		tfId.setText("");
 	}
 	protected void actionPerformedBtnNewButton_1(ActionEvent e) {
-		
+		Manager manager = inputManager(); // 입력한 정보
+		Manager manager1 = dao.selectByPass(manager);
+		if(manager1 == null) {
+			JOptionPane.showMessageDialog(null, "아이디가 없거나 비밀번호가 일치하지 않습니다.");
+		}else {
+			MainFrame frame = new MainFrame();
+			frame.setVisible(true);
+		}
+	}
+	public Manager inputManager() {
+		String id = tfId.getText();
+		char[] pass = pfPass.getPassword();
+		String password = "";
+		for(char cha : pass) {
+			password+=cha;
+		}
+		Manager manager = new Manager();
+		manager.setId(id);
+		manager.setPwd(password);
+		return manager;
 	}
 }
 
