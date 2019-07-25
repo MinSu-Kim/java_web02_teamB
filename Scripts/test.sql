aadc15d786ff6fc61e51a14e2ae2a46dbd36aa98
변지영 <bjy8668@hanmail.net>
2019-07-25 13:58:09 +0900

    foodmapper

--- Scripts/test.sql
+++ Scripts/test.sql
@@ -21,3 +21,20 @@ select f.name, f.price, fk.fk_no
 		join foodkind fk
 		on f.fk_no = fk.fk_no
 
+
+insert into sale values (1,1,now(),1,1,0,1,1);
+insert into sale values (2,1,now(),3,1,0,1,1);
+insert into sale values (3,1,now(),4,1,0,2,1);
+insert into sale values (4,1,now(),5,1,0,2,1);
+insert into sale values (5,1,now(),2,1,0,2,1);
+insert into sale values (6,1,now(),1,1,0,3,1);
+insert into sale values (7,1,now(),3,1,0,4,1);
+
+
+select @rank := @rank+1 as rank, name, count, price, ssTotalPrice, round(ssTotalPrice/sum(ssTotalPrice) * 100,1) as percent
+from(
+select f.name as name, sum(order_cnt) as count, f.price as price, sum(order_cnt*f.price) as ssTotalPrice
+from sale s left join food f on s.fd_no = f.fd_no
+group by s.fd_no
+order by ssTotalPrice desc) sub1
+cross join (select @rank:=0) sub2;
