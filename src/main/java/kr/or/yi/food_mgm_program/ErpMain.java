067e73db9cf55f4d54da73f3246479c43d81a3e5
변지영 <bjy8668@hanmail.net>
2019-07-29 17:45:35 +0900

    init변경

--- src/main/java/kr/or/yi/food_mgm_program/ErpMain.java
+++ src/main/java/kr/or/yi/food_mgm_program/ErpMain.java
@@ -13,8 +13,8 @@ public class ErpMain {
 			// select Look and Feel
 //			UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
 			
-			UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
-//			UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
+//			UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+			UIManager.setLookAndFeel("com.jtattoo.plaf.aero.AeroLookAndFeel");
 //			UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
 			
 			// start application
